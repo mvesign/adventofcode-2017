968a4512b8b60a7e5b1fefe752bae8713863845f
Maikel Vreugde <git@mvesign.com>
2017-12-15 16:53:04 +0100

    Add stub code of day 1-2

---
+++ day-1-2/code.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MVESIGN.AdventOfCode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var digits = File.ReadAllText(@"input.txt");
+
+			long captcha = 0;
+
+			//TODO
+
+			Console.WriteLine($"Captcha: {captcha}");
+		}
+	}
+}
